a4240bee9d16780a2dfe625341fe8f5d1ef081b7
Lautaro Melchiori <lautaromelchiori01@gmail.com>
2021-04-28 14:32:00 -0300

    actualizo archivo

--- clase07/fileparse.py
+++ clase07/fileparse.py
@@ -103,58 +103,53 @@ def parse_csv(nombre_archivo, select = None, types = None):
 """
 # Ejercicio 6.9
 # Lancemos errores, Atrapemos errores y ejercicio 7.1 agregados
-# Ejercicio 7.4 agregado
 
-def parse_csv(filass, select = None, types = None, has_headers = True, silence_errors = True):
+def parse_csv(f, select = None, types = None, has_headers = True, silence_errors = True):
     '''
-    Parsea datos a partir de filas de texto con valores separados por comas
+    Parsea un archivo CSV en una lista de registros.
     Se puede seleccionar sólo un subconjunto de las columnas, determinando el parámetro select, que debe ser una lista de nombres de las columnas a considerar.
     Se puede elegir el tipo de dato en que cada fila debe ser devuelta, determinando el parámetro types, que debe ser una lista de funciones de conversión (str, float, int, etc.).
     Se puede parsear un archivo sin encabezados, determinando el parametro has_headers a False, en cuyo caso la funcion devolvera una lista de tuplas, una por fila
     '''
+    
+    filas = csv.reader(f)
 
-    # Si hay encabezados en el archivo, los tomara en cuenta
+    # Si hay encabezados en el archivo, los lee
     if has_headers:
+        encabezados = next(filas)
+
+        # Si se indicó un selector de columnas,
+        # buscar los índices de las columnas especificadas.
+        # Y en ese caso achicar el conjunto de encabezados para diccionarios
+
+        if select:
+            indices = [encabezados.index(nombre_columna) for nombre_columna in select]
+            encabezados = select
+        else:
+            indices = []
+
         registros = []
-        filas = csv.reader(filass)
         for index, fila in enumerate(filas):
-            # convertimos cada fila en una lista de valores
-            # fila.replace(' ', '')
-            # fila = fila.split(',')
-
-            # Agarramos los encabezados de la primera fila
-            if index == 0:
-                encabezados = fila
-                # Si se indicó un selector de columnas,
-                # buscar los índices de las columnas especificadas.
-                # Y en ese caso achicar el conjunto de encabezados para diccionarios
-                if select:
-                    indices = [encabezados.index(nombre_columna) for nombre_columna in select]
-                    encabezados = select
-                else:
-                    indices = []
-
             if not fila:    # Saltear filas vacías
                 continue
-            else: # if index != 0: procesamos el resto de las filas
-                try:
-                    # Filtrar la fila si se especificaron columnas
-                    if indices:
-                        fila = [fila[index] for index in indices]
 
-                    # Convertir tipos de datos si se especificaron
-                    if types:
-                        fila = [func(val) for func, val in zip(types, fila)]
-                        
-                    
-                    # Armar el diccionario y agregarlo a la lista
-                    registro = dict(zip(encabezados, fila))
-                    registros.append(registro)
-                # Manejamos el error que puede surgir en caso que no podamos procesar cierta fila
-                except ValueError as error:
-                    if not silence_errors:
-                        print(f'Fila {index + 1}: No pude convertir {fila}')
-                        print(f'Fila {index + 1}: Motivo: {error}')
+            try:
+                # Filtrar la fila si se especificaron columnas
+                if indices:
+                    fila = [fila[index] for index in indices]
+
+                # Convertir tipos de datos si se especificaron
+                if types:
+                    fila = [func(val) for func, val in zip(types, fila)]
+                
+                # Armar el diccionario y agregarlo a la lista
+                registro = dict(zip(encabezados, fila))
+                registros.append(registro)
+            # Manejamos el error que puede surgir en caso que no podamos procesar cierta fila
+            except ValueError as error:
+                if not silence_errors:
+                    print(f'Fila {index + 1}: No pude convertir {fila}')
+                    print(f'Fila {index + 1}: Motivo: {error}')
             
     # Si no hay encabezados, devolveremos una lista de tuplas representando la informacion de las filas
     else:
@@ -170,7 +165,7 @@ def parse_csv(filass, select = None, types = None, has_headers = True, silence_e
             try:
                 # Convertir tipos de datos si se especificaron
                 if types:
-                    fila = [func(val) for func, val in zip(types, fila.split(','))]
+                    fila = [func(val) for func, val in zip(types, fila)]
 
                 # Armar la tupla y agregarla a la lista
                 registro = tuple(fila)
@@ -180,5 +175,5 @@ def parse_csv(filass, select = None, types = None, has_headers = True, silence_e
                 if not silence_errors:
                     print(f'Fila {index + 1}: No pude convertir {fila}')
                     print(f'Fila {index + 1}: Motivo: {error}')
-                    
+                
     return registros
