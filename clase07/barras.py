01a33d8f9985dfa08d93547e0c2a46abcb230785
lauta <lautaromelchiori01@gmail.com>
2021-05-06 23:06:25 -0300

    actualizo archivo

--- clase07/barras.py
+++ clase07/barras.py
@@ -10,6 +10,7 @@ Y2 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
 plt.bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
 plt.bar(X, -Y2, facecolor='#ff9999', edgecolor='white')
 
+
 for x, y in zip(X, Y1):
     plt.text(x + 0.4, y + 0.05, '%.2f' % y, ha='right', va='bottom')
 
