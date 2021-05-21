d6c202857c567c22ff55379bf4f9e799a10e207e
Lauty <lautaromelchiori01@gmail.com>
2021-05-21 17:39:57 -0300

    añado archivo

---
+++ clase10/camion.py
@@ -0,0 +1,16 @@
+# camion.py
+
+class Camion:
+
+    def __init__(self, lotes):
+        self.lotes = lotes
+
+    def precio_total(self):
+        return sum([l.costo() for l in self.lotes])
+
+    def contar_cajones(self):
+        from collections import Counter
+        cantidad_total = Counter()
+        for l in self.lotes:
+            cantidad_total[l.nombre] += l.cajones
+        return cantidad_total
