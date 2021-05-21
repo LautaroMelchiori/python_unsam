a98b7d5381f301cecdfc866eed9c20f4e16620a5
Lauty <lautaromelchiori01@gmail.com>
2021-05-21 17:47:32 -0300

    añado archivo de clase pasada para modificar

---
+++ clase10/costo_camion.py
@@ -0,0 +1,20 @@
+# costo_camion.py
+import informe
+
+# Ejercicio 6.12
+
+def costo_camion(archivo_camion):
+    camion = informe.leer_camion(archivo_camion)
+    total = 0
+    for lote in camion:
+        total += lote.costo()
+    print(f'Costo total {total}')
+
+# Ejercicio 7.2 y 7.3
+
+def main(parametros):
+    costo_camion(parametros[1])
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
