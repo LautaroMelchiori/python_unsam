3e219d9e514775bfa0258f60f8d6a70a3101d3cd
Lauty <lautaromelchiori01@gmail.com>
2021-05-23 16:15:53 -0300

    actualizo archivo ej. 10.16

--- clase10/listar_imgs.py
+++ clase10/listar_imgs.py
@@ -1,9 +1,21 @@
+import datetime
 import os
 import sys
 
 path = sys.argv[1]
 
-for root, dirs, files in os.walk(path):
+def generador(root, files):
     for f in files:
         if f[-4:] == '.png':
-            print(f)
+            viejos = f'{root}/{f}'
+
+            nuevo_nombre = f[:-13] + '.png'
+            nuevos = f'imgs_procesadas/{nuevo_nombre}'
+
+            fecha = datetime.datetime.strptime(f[-12:-4], '%Y%m%d')
+
+            yield (viejos, nuevos, fecha)
+
+for root, dirs, files in os.walk(path):
+    for f in generador(root, files):
+        print(f)
