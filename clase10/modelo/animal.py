cb6a6d538dc87291c52713cfb49b1f9c4fb8dd77
Lauty <lautaromelchiori01@gmail.com>
2021-05-25 19:29:22 -0300

    actualizo archivo: animales solo pueden reproducirse 1 vez por ciclo, por default tienen 1 sola reproduccion, solo pueden reproducirse con animales de la misma especie

--- clase10/modelo/animal.py
+++ clase10/modelo/animal.py
@@ -11,17 +11,25 @@ class Animal(object):
     """docstring for Animal"""
     def __init__(self):
         super(Animal, self).__init__()
-        self.reproducciones_pendientes = 4
+        self.reproducciones_pendientes = 1
+        self.especie = None
         self.edad = 0
         self.sexo = None # Posible mejora para que no se puedan reproducir dos del mismo sexo
         self.energia = self.energia_maxima
+        self.se_reprodujo_este_ciclo = False
         self.es_reproductore = False
 
     def pasar_un_ciclo(self):
         self.energia -= 1 # Se puede restar si no llega a comer
         self.edad += 1
-        if self.reproducciones_pendientes > 0: #
+
+        # reseteamos la variable para el proximo ciclo
+        self.se_reprodujo_este_ciclo = False
+
+        if self.reproducciones_pendientes > 0:
             self.es_reproductore = True
+        else:
+            self.es_reproductore = False
 
     def en_vida(self):
         return (self.edad <= self.edad_maxima) and self.energia > 0
@@ -29,8 +37,10 @@ class Animal(object):
     def tiene_hambre(self):
         """Acá se puede poner comportamiento para que no tenga hambre todo el tiempo
         debería depender de la diferencia entre su nivel de energía y su energía máxima"""
-        return True
-        #pass
+        if self.energia < self.energia_maxima - 1:
+            return True
+        else:
+            return False
 
     def es_leon(self):
         return False
@@ -39,20 +49,25 @@ class Animal(object):
         return False
 
     def puede_reproducir(self):
-        return self.es_reproductore
+        return self.es_reproductore and not self.se_reprodujo_este_ciclo and (self.edad >= 2)
 
     def tener_cria(self):
         """Acá se puede poner comportamiento que sucede al tener cria para evitar que tengamás de una cria por ciclo, etc"""
         self.reproducciones_pendientes -= 1
-        # pass
+        self.se_reprodujo_este_ciclo = True
 
     def reproducirse(self, vecinos, lugares_libres):
         pos = None
         if vecinos:
-            animal = random.choice(vecinos)
-            if lugares_libres:
+            # filtramos solo animales de la misma especie
+            vecinos_misma_especie = [v[1] for v in vecinos if v[1].es_leon() == self.es_leon()]
+            
+            if vecinos_misma_especie and lugares_libres:
+                animal = random.choice(vecinos_misma_especie)
+
                 animal.tener_cria()
                 self.tener_cria()
+
                 pos = random.choice(lugares_libres)
 
         return pos
