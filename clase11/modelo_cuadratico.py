218300b5de00f2b18b510c23c7b3ba0fbe01d269
lauta <lautaromelchiori01@gmail.com>
2021-06-01 23:13:41 -0300

    actualizo archivo: añado labels a los plots y comentarios

--- clase11/modelo_cuadratico.py
+++ clase11/modelo_cuadratico.py
@@ -26,53 +26,61 @@ lm = linear_model.LinearRegression()
 grilla_x = np.linspace(start = 0, stop = 10, num = 1000)
 
 
+# REGRESION SIMPLE CON X COMO REGRESOR
+######################################
 lm.fit(x.reshape(-1, 1), y)
 
 a = lm.coef_[0]
 b = lm.intercept_
 
-print('Regresion lineal con x como regresor')
+print('Regresion simple con x como regresor')
 print(f'a = {a}, b = {b}')
 
 errores = y - lm.predict(x.reshape(-1,1))
 print(f'ECM: {(errores ** 2).mean()}')
 
 grilla_y = a * grilla_x + b
-plt.plot(grilla_x, grilla_y, c = 'brown')
+plt.plot(grilla_x, grilla_y, c = 'green', label = 'Regresion simple con x como regresor')
 
 
+# REGRESION SIMPLE CON X² COMO REGRESOR
+#######################################
 lm.fit(xc.reshape(-1, 1), y)
 
 a = lm.coef_[0]
 b = lm.intercept_
 
 print()
-print('Regresion lineal con x² como regresor')
+print('Regresion simple con x² como regresor')
 print(f'a = {a}, b = {b}')
 
 errores = y - lm.predict(xc.reshape(-1,1))
 print(f'ECM: {(errores ** 2).mean()}')
 
 grilla_y = a * (grilla_x**2) + b
-plt.plot(grilla_x, grilla_y, c = 'green')
+plt.plot(grilla_x, grilla_y, c = 'yellow', label = 'Regresion simple con x² como regresor')
 print()
 
+# REGRESION  MULTIPLE CON X E X² COMO REGRESORES
+################################################
 lm.fit(X, y)
 
 a = lm.coef_[1]
 b = lm.coef_[0]
 c = lm.intercept_
 
-print('Regresion multiple con x e x² como regresores')
+print('Regresion multiple con x y x² como regresores')
 print(f'a = {a}, b = {b}, c = {c}')
 
 errores = y - lm.predict(X)
 print(f'ECM: {(errores ** 2).mean()}')
 
 grilla_y = a * (grilla_x ** 2) + (b * grilla_x) + c
-plt.plot(grilla_x, grilla_y, c = 'red')
+plt.plot(grilla_x, grilla_y, c = 'red', label = 'Regresion multiple con x y x² como regresores')
 
 
 plt.scatter(x = x , y = y)
+
+plt.legend()
 
 plt.show()
