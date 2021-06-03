fb13a9cf16f1b821c36a5899221a7186ec8fb6f4
lauta <lautaromelchiori01@gmail.com>
2021-06-03 08:03:49 -0300

    añado archivo ej. 12.2

---
+++ clase12/burbujeo.py
@@ -0,0 +1,19 @@
+# ejercicio 12.2
+
+def ord_burbujeo(lista):
+    """
+    Ordena una lista de elementos según el método de burbujeo.
+    Pre: los elementos de la lista deben ser comparables.
+    Post: la lista está ordenada.
+    """
+    comparaciones = 0
+    for i in range(1, len(lista)):
+        for j in range(len(lista) - i):
+            comparaciones += 1
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    return comparaciones
+            
+# La complejidad de este algoritmo es de O(n²), 
+# ya que mediante los ciclos anidados recorre n veces la lista de longitud n.
