4bc6978605a13b6aea42c22f31a196f9ab31bb92
lauta <lautaromelchiori01@gmail.com>
2021-06-08 17:29:58 -0300

    añado archivo ej. 12.10

---
+++ clase12/iris_seaborn.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.datasets import load_iris
+
+iris_dataset = load_iris()
+
+# creamos un dataframe de los datos de flores
+# etiquetamos las columnas usando las cadenas de iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(iris_dataset['data'],
+                              columns=iris_dataset.feature_names)
+
+iris_dataframe['target'] = iris_dataset['target']
+
+# y hacemos una matriz de gráficos de dispersión, asignando colores según la especie
+sns.pairplot(iris_dataframe, hue='target', diag_kind="hist")
+
+plt.show()
