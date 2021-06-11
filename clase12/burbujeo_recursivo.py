60af59d5bad503c938db2770d8165fe94339da8b
lauta <lautaromelchiori01@gmail.com>
2021-06-11 14:50:04 -0300

    añado contador de comparaciones a burbujeo recursivo (ahora si)

--- clase12/burbujeo_recursivo.py
+++ clase12/burbujeo_recursivo.py
@@ -3,9 +3,10 @@ def ord_burbujeo(lista, idx=1, comps=0):
     Recibe una lista y la ordena
     """
     if idx == len(lista):
-        return lista
+        return lista, comps
 
     for j in range(len(lista) - idx):
+        comps += 1
         if lista[j] > lista[j + 1]:
             lista[j], lista[j + 1] = lista[j + 1], lista[j]
 
